44e3c5c7935f8ee2a905f2dbf1b62e26d980cfbc
Dmitri Sholomitski <d.sholomitski@outlook.com>
2024-10-10 14:42:51 +0300

    updates

--- utils/validation/deployments.py
+++ utils/validation/deployments.py
@@ -11,8 +11,8 @@ def validate_deployment_file_content(deployment_file_content: dict):
     for deployment in deployment_file_content['deployments']:
         for attribute in ('name', 'description', 'entrypoint', 'work_pool'):
             assert deployment.get(attribute) is not None, f'You should set `{attribute}` in each deployment'
-        assert deployment.get('collision_strategy') == 'CANCEL_NEW', 'You should set collision_strategy=CANCEL_NEW'
-        assert deployment.get('concurrency_limit') == 1, 'You should set concurrency_limit=1'
+        assert deployment.get('collision_strategy') == 'CANCEL_NEW', 'You should set `collision_strategy=CANCEL_NEW`'
+        assert deployment.get('concurrency_limit') == 1, 'You should set `concurrency_limit=1`'
 
 
 def validate_deployment_file(filepath: str):
